4aa4f8b12b58490b4dd6deb10edac6a7c9f7ed03
reeboo <ruibo.ma@qunar.com>
2016-08-19 09:50:45 +0800

    config logback

--- src/main/java/com/fun/util/vote/VoteUtil.java
+++ src/main/java/com/fun/util/vote/VoteUtil.java
@@ -29,7 +29,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class VoteUtil {
 
     static Logger logger = LoggerFactory.getLogger(VoteUtil.class);
-    static final ThreadPoolExecutor executor = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors() * 20
+    static final ThreadPoolExecutor executor = new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors()
             , Runtime.getRuntime().availableProcessors() * 20,
             60L, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>(1000), new ThreadPoolExecutor.CallerRunsPolicy());
 
